c8645bdc6edd18bf42e036b1d318a02bd9df0d8d
taldoRafa <rafavianni@gmail.com>
2026-04-28 16:26:32 -0300

    Usuario.java

--- codigos A3/Usuario.java
+++ codigos A3/Usuario.java
@@ -1,15 +1,28 @@
 public class Usuario {
 
-String email;
+    private String email;
+    private String senha;
 
-String senha;
-
- public Usuario(String email, String senha) {
+    public Usuario(String email, String senha) {
         this.email = email;
         this.senha = senha;
     }
- public Usuario(){
 
- }
-    
+    public Usuario() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
